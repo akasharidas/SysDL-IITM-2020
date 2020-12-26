7bb05d67281959450c0d5f44c0f79a9823557c20
Akash Haridas <akash.haridas@gmail.com>
2020-12-26 10:39:21 +0530

    added wandb logging

--- assignment_conv/run_benchmark.py
+++ assignment_conv/run_benchmark.py
@@ -4,6 +4,7 @@ from statistics import mean
 from collections import defaultdict
 from tqdm import tqdm
 import json
+import wandb
 
 from direct import Direct_Conv2d
 from im2col import Im2Col_Conv2d
@@ -15,7 +16,7 @@ from decomp import CP_Conv2d
 
 def run_and_time(name, model, _input, _filter):
     times = []
-    iters = 1 if name == "direct" else 1
+    iters = 1 if name == "direct" else 5
 
     for _ in tqdm(range(iters), desc=f"Running benchmark for {name}"):
         start_time = time.time()
@@ -37,31 +38,33 @@ def test_implementation(name, model, _input, _filter):
     return torch.allclose(reference, model(_input, _filter))
 
 
-if __name__ == "__main__":
-    convs = {
-        "direct": Direct_Conv2d,
-        "im2col": Im2Col_Conv2d,
-        "winograd": Winograd_Conv2d,
-        "fft": FFT_Conv2d,
-        "tucker": Tucker_Conv2d,
-        "cp": CP_Conv2d,
-    }
+def run(config, convs, devices, test=False):
+    wandb.init(project="sysdl-assignment7")
 
-    devices = ["cpu", "cuda"]
     results = defaultdict(dict)
-
-    N, C, H, W, M, R, S = 8, 3, 32, 32, 2, 3, 3
+    N, C, H, W, M, R, S = (
+        config["N"],
+        config["C"],
+        config["H"],
+        config["W"],
+        config["M"],
+        config["R"],
+        config["S"],
+    )
     test_i = torch.rand(N, C, H, W, dtype=torch.float)
     test_f = torch.rand(M, C, R, S, dtype=torch.float)
 
     with torch.no_grad():
 
-        # check that the outputs match the reference PyTorch implementation
-        print("Testing correctness of implementations...")
-        for name, conv2d in convs.items():
-            model = conv2d(C, M, R, S, test_f) if name in ["tucker", "cp"] else conv2d()
-            assert test_implementation(name, model, test_i, test_f)
-        print("Tests passed.")
+        if test:
+            # check that the outputs match the reference PyTorch implementation
+            print("Testing correctness of implementations...")
+            for name, conv2d in convs.items():
+                model = (
+                    conv2d(C, M, R, S, test_f) if name in ["tucker", "cp"] else conv2d()
+                )
+                assert test_implementation(name, model, test_i, test_f)
+            print("Tests passed.")
 
         for device in devices:
             print(f"Running benchmarks on {device}...")
@@ -71,7 +74,23 @@ if __name__ == "__main__":
                     conv2d(C, M, R, S, test_f) if name in ["tucker", "cp"] else conv2d()
                 )
                 model.to(device)
-                results[device][name] = run_and_time(name, model, test_i, test_f)
+                results[name][device] = run_and_time(name, model, test_i, test_f)
 
-    with open("results.json", "w") as f:
-        json.dump(dict(results), f, indent=4)
+    wandb.log({"results": dict(results), "config": config})
+
+
+if __name__ == "__main__":
+    wandb.login(key="df416cf0e6b9361efc64aa08d4715af979c8d070")
+
+    convs = {
+        "im2col": Im2Col_Conv2d,
+        "winograd": Winograd_Conv2d,
+        "fft": FFT_Conv2d,
+        "tucker": Tucker_Conv2d,
+        "cp": CP_Conv2d,
+        "direct": Direct_Conv2d,
+    }
+    devices = ["cpu", "cuda"]
+
+    config = dict(N=8, C=3, H=32, W=32, M=2, R=3, S=3)
+    run(config, convs, devices)
